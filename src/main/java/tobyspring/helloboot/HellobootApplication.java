4c4231f3a707a2108244866c3d64c5685de08487
Toby Lee <tobyilee@gmail.com>
2022-12-05 05:49:51 +1000

    서블릿 컨테이너(Tomcat) 실행 코드 추가

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,15 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
+    }
 
 }
