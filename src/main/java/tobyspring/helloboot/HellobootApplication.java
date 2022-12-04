d7ad5746fa195e051a3abbd5074be2ae458d4a15
Toby Lee <tobyilee@gmail.com>
2022-12-05 05:58:06 +1000

    서블릿 요청 처리

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -22,9 +22,11 @@ public class HellobootApplication {
             servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
                     resp.setStatus(HttpStatus.OK.value());
                     resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("Hello Servlet");
+                    resp.getWriter().println("Hello " + name);
                 }
             }).addMapping("/hello");
         });
