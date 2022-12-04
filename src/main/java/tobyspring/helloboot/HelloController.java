22f6ba280a43e1329d8d4da2bf22f6a4895bdd42
Toby Lee <tobyilee@gmail.com>
2022-12-05 07:48:19 +1000

    Hello 컨트롤러 매핑과 바인딩

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,6 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
-    @GetMapping("/hello")
     public String hello(String name) {
         return "Hello " + name;
     }
