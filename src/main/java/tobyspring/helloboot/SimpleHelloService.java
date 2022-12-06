b7d2327ec4a19c56de85aaf66e7d6a74ebd2081c
Toby Lee <tobyilee@gmail.com>
2022-12-06 17:33:30 +1000

    의존 오브젝트 DI 적용

--- src/main/java/tobyspring/helloboot/SimpleHelloService.java
+++ src/main/java/tobyspring/helloboot/SimpleHelloService.java
@@ -1,7 +1,8 @@
 package tobyspring.helloboot;
 
-public class SimpleHelloService {
-    String sayHello(String name) {
+public class SimpleHelloService implements HelloService {
+    @Override
+    public String sayHello(String name) {
         return "Hello " + name;
     }
 }
