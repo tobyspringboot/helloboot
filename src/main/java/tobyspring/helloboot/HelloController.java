916f9925e66e575d1f73e9b813177abe78e99bb1
Toby Lee <tobyilee@gmail.com>
2022-12-06 17:31:48 +1000

    의존 오브젝트 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,11 @@
 package tobyspring.helloboot;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
