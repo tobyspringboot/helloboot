d62bc29b1024f43276890eb410364bde611636f3
Toby Lee <tobyilee@gmail.com>
2022-12-08 17:40:54 +1000

    MySpringApplication
    MySpringApplication

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,7 @@
 package tobyspring.helloboot;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
@@ -18,8 +14,6 @@ public class HelloController {
     public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
         this.applicationContext = applicationContext;
-
-        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
