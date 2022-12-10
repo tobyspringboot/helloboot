9f13207cc76cfc4f2f8481c5e12aa320f430d60e
Toby Lee <tobyilee@gmail.com>
2022-12-10 13:22:02 +1000

    테스트 코드를 이용한 테스트, DI와 단위 테스트

---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,35 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+
+    @Test
+    void failsHelloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/test/java/tobyspring/helloboot/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloControllerTest {
+    @Test
+    void helloController() {
+        HelloController helloController = new HelloController(name -> name);
+
+        String ret = helloController.hello("Test");
+
+        Assertions.assertThat(ret).isEqualTo("Test");
+    }
+
+    @Test
+    void failsHelloController() {
+        HelloController helloController = new HelloController(name -> name);
+
+        Assertions.assertThatThrownBy(() -> {
+            helloController.hello(null);
+        }).isInstanceOf(IllegalArgumentException.class);
+
+        Assertions.assertThatThrownBy(() -> {
+            helloController.hello("");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
