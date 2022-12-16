4d0cbfdab2aac7a7c9a45c7ca092650e25d748f3
Toby Lee <tobyilee@gmail.com>
2022-12-16 16:27:10 +1000

    @Configuration과 proxyBeanMethods 학습 테스트

---
+++ src/main/java/tobyspring/config/MyAutoConfiguration.java
@@ -0,0 +1,14 @@
+package tobyspring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Configuration(proxyBeanMethods = false)
+public @interface MyAutoConfiguration {
+}
--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,8 +4,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tobyspring.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
