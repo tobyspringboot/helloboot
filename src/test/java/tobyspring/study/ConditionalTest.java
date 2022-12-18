731fe7a79c65bd8a76a8783f0023998b8c0731d8
Toby Lee <tobyilee@gmail.com>
2022-12-18 14:17:50 +1000

    @Conditional과 Condition 적용, 학습 테스트 작성

---
+++ src/test/java/tobyspring/study/ConditionalTest.java
@@ -0,0 +1,72 @@
+package tobyspring.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConditionalTest {
+    @Test
+    void conditional() {
+        // true
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+        contextRunner.withUserConfiguration(Config1.class)
+                .run(context -> {
+                    assertThat(context).hasSingleBean(MyBean.class);
+                    assertThat(context).hasSingleBean(Config1.class);
+                });
+
+        // false
+        new ApplicationContextRunner().withUserConfiguration(Config2.class)
+                .run(context -> {
+                    assertThat(context).doesNotHaveBean(MyBean.class);
+                    assertThat(context).doesNotHaveBean(Config1.class);
+                });
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(BooleanCondition.class)
+    @interface BooleanConditional {
+        boolean value();
+    }
+
+    @Configuration
+    @BooleanConditional(true)
+    static class Config1 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+
+    @Configuration
+    @BooleanConditional(false)
+    static class Config2 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+
+    static class MyBean {}
+
+    static class BooleanCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(BooleanConditional.class.getName());
+            Boolean value = (Boolean)annotationAttributes.get("value");
+            return value;
+        }
+    }
+
+}
