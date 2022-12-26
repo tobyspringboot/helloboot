edd50cf35ac5bf44bbd06fbc496f2bde34ad68e1
Toby Lee <tobyilee@gmail.com>
2022-12-27 07:43:01 +1000

    프로퍼티 빈 후처리기와 @EnableMyConfigurationProperties 도입

---
+++ src/main/java/tobyspring/config/autoconfig/PropertyPostProcessorConfig.java
@@ -0,0 +1,32 @@
+package tobyspring.config.autoconfig;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.env.Environment;
+import tobyspring.config.MyAutoConfiguration;
+import tobyspring.config.MyConfigurationProperties;
+
+import java.util.Map;
+
+import static org.springframework.core.annotation.AnnotationUtils.*;
+
+@MyAutoConfiguration
+public class PropertyPostProcessorConfig {
+    @Bean BeanPostProcessor propertyPostProcessor(Environment env) {
+        return new BeanPostProcessor() {
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                MyConfigurationProperties annotation = findAnnotation(bean.getClass(), MyConfigurationProperties.class);
+                if (annotation == null) return bean;
+
+                Map<String, Object> attrs = getAnnotationAttributes(annotation);
+                String prefix = (String) attrs.get("prefix");
+
+                return Binder.get(env).bindOrCreate(prefix, bean.getClass());
+            }
+        };
+    }
+}
--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,18 +1,16 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
+import org.springframework.context.annotation.Bean;
 import tobyspring.config.ConditionalMyOnClass;
+import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
